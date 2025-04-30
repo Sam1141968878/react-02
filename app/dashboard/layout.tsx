cbeb4a453a48f27dbc8bbe1846f75ccf9411732d
duke <dukelin@anycubic.com>
2025-04-30 11:17:11 +0800

    1

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: duke
+ * @Date: 2025-04-27 15:03:54
+ * @Description: 在此处清晰,简洁的说明本文件的作用
+ */
+import SideNav from '@/app/ui/dashboard/sidenav';
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: duke
+ * @Date: 2025-04-27 14:08:43
+ * @Description: 在此处清晰,简洁的说明本文件的作用
+ */
+import { Inter,Lusitana  } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
